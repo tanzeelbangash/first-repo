625652cd29f970f92b1f4a387ccc5877423e8987
tanzeelbangash <tanzeelabbas8805@gmail.com>
2025-05-01 05:18:44 +0500

    changing the cout by naming

--- ICT Final project/test.cpp
+++ ICT Final project/test.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"HEllo";
+    cout<<"Hey I am tanzeel";
+    cout<<"This code is written by me";
     return 0;
 }
--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int main(){
     cout<<"Tanzeel is here";
+    cout<<"HEllo";
     return 0;
 }
